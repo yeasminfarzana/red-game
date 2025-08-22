d5c7c32e8685c742e90c9c3b66cf5b5d5ba5e5aa
yeasminfarzana <yeasmin.oni@gmail.com>
2025-08-22 10:30:20 +0100

    added index, styles

---
+++ game.js
@@ -0,0 +1,17 @@
+const textElement = document.getElementById("text");
+const optionButtons = document.getElementById("optionButtons");
+
+let state = {};
+
+/* Function to start game */
+function startGame() {
+  state = {};
+  showTextNode(1);
+}
+
+/*Take particular index*/
+function showTextNode(textNodeIndex) {}
+
+function selectOption() {}
+
+startGame();
